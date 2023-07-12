998706789be997329f584f663f823d4322fd1f42
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-07-12 08:03:23 -0600

    Finished the Exercise14-15 code

--- CSCI_1112_Mod1/src/Exercise14_15.java
+++ CSCI_1112_Mod1/src/Exercise14_15.java
@@ -3,6 +3,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -12,7 +13,14 @@ public class Exercise14_15 extends Application {
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {   
 		// Create a scene and place it in the stage
-		Scene scene = new Scene(new MyPolygon(), 400, 400);
+		Pane plane = new StackPane();
+		plane.getChildren().add(new MyPolygon());
+		Label label = new Label("STOP");
+		label.setTextFill(Color.WHITE);
+		label.setFont(Font.font("Times New Roman", 80));
+		plane.getChildren().add(label);
+		Scene scene = new Scene(plane, 400, 400);
+		//Scene scene = new Scene(new MyPolygon(), 400, 400);
 		primaryStage.setTitle("StopSign"); // Set the stage title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
@@ -46,13 +54,9 @@ class MyPolygon extends Pane {
 			list.add(centerX + radius * Math.sin(2 * i * Math.PI / s - 20.03)); 
 			list.add(centerY - radius * Math.cos(2 * i * Math.PI / s - 20.03));
 		}     
-		Label label = new Label("STOP");
-		label.setTextFill(Color.WHITE);
-		label.setFont(Font.font("Times New Roman", 80));
-		//getChildren().add(label);
 		
 		getChildren().clear();
-		getChildren().addAll(polygon, label);
+		getChildren().add(polygon);
 		
 		
 		
