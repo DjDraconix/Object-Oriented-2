c183361370470cf4d366eca9f3bca1492cef9b1d
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-08-07 10:38:19 -0600

    Skeleton of Exercise17-encrypt made

--- CSCI_1112_Mod3/src/Exercise17_Encryption.java
+++ CSCI_1112_Mod3/src/Exercise17_Encryption.java
@@ -1,5 +1,7 @@
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -30,24 +32,83 @@ public class Exercise17_Encryption {
 			inFile = new FileInputStream(fileName + ".dat");
 		}
 		
-		inFile.close();
-		in.close();
+		System.out.println("Please enter the name of a file to push the encryption to"
+				+ "\nFor pre made file enter File");
+
+		fileName = in.next();
+		FileOutputStream outFile;
+		if (fileName.equals("File")) {
+			outFile = new FileOutputStream("Exercise17_14enc.dat");
+		} else {
+			outFile = new FileOutputStream(fileName + ".dat");
+		}
+		
+		try{
+			while (true) {
+				int encrypt = inFile.read();
+				encrypt += 5;
+				outFile.write(encrypt);
+			}
+		}
+		catch(EOFException ex) {
+			outFile.close();
+			inFile.close();
+			in.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Did not work part 1");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Did not work part 2");
+			e.printStackTrace();
+		}
+		
+		System.out.println("Encryption compleate");
 	}
 
 	public static void Exercise17_15() throws IOException {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Please enter the name of a file to pull from to encrypt"
+		System.out.println("Please enter the name of a file to pull from to decryptcrypt"
 				+ "\nFor pre made file enter File");
 		String fileName = in.next();
 		FileInputStream inFile;
 		if (fileName.equals("File")) {
-			inFile = new FileInputStream("Exercise17_14.dat");
+			inFile = new FileInputStream("Exercise17_14enc.dat");
 		} else {
 			inFile = new FileInputStream(fileName + ".dat");
 		}
 		
-		inFile.close();
-		in.close();
+		System.out.println("Please enter the name of a file to push the decryption to"
+				+ "\nFor pre made file enter File");
+		fileName = in.next();
+		FileOutputStream outFile;
+		if (fileName.equals("File")) {
+			outFile = new FileOutputStream("Exercise17_14.dat");
+		} else {
+			outFile = new FileOutputStream(fileName + ".dat");
+		}
+		
+		try{
+			while (true) {
+				int decrypt = inFile.read();
+				decrypt -= 5;
+				outFile.write(decrypt);
+			}
+		}
+		catch(EOFException ex) {
+			outFile.close();
+			inFile.close();
+			in.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Did not work part 1");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Did not work part 2");
+			e.printStackTrace();
+		}
+
+		System.out.println("Decryption compleate");
 	}
 	
 }
