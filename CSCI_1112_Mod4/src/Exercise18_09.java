ac9bb926588250adc59099f483e893c27041e243
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-08-09 08:52:13 -0600

    Fixed Mod 4

--- CSCI_1112_Mod4/src/Exercise18_09.java
+++ CSCI_1112_Mod4/src/Exercise18_09.java
@@ -2,8 +2,18 @@
 public class Exercise18_09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String string = "gnikrow";
+		reverceDisplay(string);
 	}
 
+	private static void reverceDisplay(String value) {
+		if (!value.isEmpty()) {
+			String flipper = new String();
+			for (int i = 0; i < value.length() - 1; i++) {
+				flipper = flipper + value.charAt(i);
+			}
+			System.out.print(value.charAt(value.length() - 1));
+			reverceDisplay(flipper);
+		}
+	}	
 }
