c77ba0519e1164e55fa47d4d546ddbb6c2e93ae5
404kevnotfound <137447786+DjDraconix@users.noreply.github.com>
2023-08-25 09:38:05 -0600

    Has moduel use level and length

--- CSCI_1112_Final/src/Module.java
+++ CSCI_1112_Final/src/Module.java
@@ -6,11 +6,21 @@ public class Module {
 	static String Name;
 	static File Explanation;
 	static Image Cover;
+	static int Length;
+	//1- one or two sessions
+	//3- a few months of sessions
+	//5- a years worth of sessions
+	static int Level;
+	//1- new players
+	//3- players no how to play decently
+	//5- players are seasoned and know most of the rules
 	
-	Module(String book){
+	public Module(String book, int time, int experiance){
 		Name = book;
 		Cover = new Image(book + ".gif");
 		Explanation = new File(book + ".txt");
+		Length = time;
+		Level = experiance;
 	}
 
 	@SuppressWarnings("unused")
@@ -26,6 +36,16 @@ public class Module {
 	@SuppressWarnings("unused")
 	private static String getName() {
 		return Name;
+	}
+	
+	@SuppressWarnings("unused")
+	private static int getLength() {
+		return Length;
+	}
+	
+	@SuppressWarnings("unused")
+	private static int getLevel() {
+		return Level;
 	}
 	
 }
